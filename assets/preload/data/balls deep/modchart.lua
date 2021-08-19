6f5e43bce87c073f5aaf18eb74a75dfd753e3794
Lexicord <84813757+Lexicord@users.noreply.github.com>
2021-08-19 16:38:13 -0500

    Initial commit

---
+++ assets/preload/data/balls deep/modchart.lua
@@ -0,0 +1,23 @@
+function start(song) -- do nothing
+
+end
+
+function update(elapsed)
+
+end
+
+function beatHit(beat) -- do nothing
+
+end
+
+function stepHit(step) -- do nothing
+    if step == 120 then
+        tweenCameraZoom(2.5,(crochet * 4) / 1000)
+    end
+end
+
+function playerTwoTurn()
+end
+
+function playerOneTurn()
+end
